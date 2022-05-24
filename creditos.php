954053a9e17d3626f273f12c34306fa7b0441802
janethcj <78242681+janethcj@users.noreply.github.com>
2022-05-24 17:21:22 -0500

    Create creditos.php
    Nombre de quiénes elaboraron el proyecto

---
+++ creditos.php
@@ -0,0 +1,70 @@
+<?php
+/*
+@name: creditos.php
+
+@authors:
+  1. Cano Jiménez Janeth
+  2. Parra Huerta Mario
+  3. Saldate Luna Luis Omar
+  4. Ortega Hernandez Ariana Jatziri
+
+@Description: Nombre de quién elaboro el proyecto
+
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Créditos</title>
+    <!-- Custom styles for this template -->
+    <link href="css/login.css" rel="stylesheet">
+
+    <style>
+    .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+    }
+
+    @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+            font-size: 3.5rem;
+        }
+    }
+    </style>
+</head>
+  <body>
+    <!-- ========== HEADER ========== -->
+    <header>
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top nav.navbar ">
+           <!--MENÚ-->
+           <div class="collapse navbar-collapse" id="navbarCollapse">
+             <ul class="navbar-nav mr-auto">
+               <li class="nav-item active">
+                   <a class="nav-link" href="catalogo.php">Catálogo</a>
+               </li>
+             <ul>
+           </div>
+           <!--FIN MENÚ-->
+       </nav>
+     </header><br><br><br><br><br>
+     <!-- ========== END HEADER ========== -->
+
+    <div class="">
+      <p>Janeth Cano Jiménez</p>
+      <p><strong><em>Estudiante Facultad de Contaduría y Administración</em></strong></p>
+      <p>Mario Parra Huerta</p>
+      <p><strong><em>Estudiante Facultad de Contaduría y Administración</em></strong></p>
+      <p>Luis Omar Saldate Luna</p>
+      <p><strong><em>Estudiante Facultad de Contaduría y Administración</em></strong></p>
+      <p>Ariana Jatziri Ortega Hernández</p>
+      <p><strong><em>Estudiante Facultad de Contaduría y Administración</em></strong></p>
+    </div>
+  </body>
+</html>

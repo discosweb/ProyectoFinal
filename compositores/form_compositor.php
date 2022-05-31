70fbec8a313125debe83fc7db9fd68a211649440
janethcj <78242681+janethcj@users.noreply.github.com>
2022-05-31 00:18:47 -0500

    validación compositores

--- compositores/form_compositor.php
+++ compositores/form_compositor.php
@@ -112,16 +112,16 @@ session_start();
 			<div class="form-group">
 
 			<label for="nombre">Nombre:</label>
-			<input class="form-control" type="text" name="nombre">
+			<input required pattern="^[a-zA-Zá-úÁ-ÚÑñ ]{2,50}$" class="form-control" type="text" name="nombre">
 
 			<label for="apellido">Apellido:</label>
-			<input class="form-control" type="text" name="apellido">
+			<input required pattern="[a-zA-Zá-úÁ-ÚÑñ ]{2,50}$" class="form-control" type="text" name="apellido">
 
 			<label for="pais_nacimiento">País de Nacimiento:</label>
-			<input class="form-control" type="text" name="pais_nacimiento">
+			<input required pattern="[a-zA-Zá-úÁ-ÚÑñ ]{2,50}$" class="form-control" type="text" name="pais_nacimiento">
 
 			<label for="fecha_nacimiento">Fecha de Nacimiento:</label>
-			<input class="form-control" type="date" name="fecha_nacimiento">
+			<input class="form-control" type="date" required name="fecha_nacimiento">
 			</div>
 			<input class="btn btn-success"type="submit" value="Enviar">
 		</form>

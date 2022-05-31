f6765f00678905349d12e2ccd8fd17b00f7fed1d
Arianna Jatziri Ortega Hernández <79209978+ariannaOrtega@users.noreply.github.com>
2022-05-30 21:06:18 -0500

    validaciones

--- compositores/form_compositor.php
+++ compositores/form_compositor.php
@@ -9,6 +9,11 @@ session_start();
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+<script src="js/jquery-3.6.0.js"></script>
+<script src="js/01.js"></script>
+<script src="js/jquery.validate.min.js"></script>
+<script src="js/validar.js"></script>
+
 <title>Compositores</title>
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
